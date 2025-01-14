525270c6baa8c61df7b89a0bc54f640902589e76
Jose Megía Guillén <jmgm62@alu.ua.es>
2025-01-14 22:54:04 +0100

    Job Cobro de cuotas

---
+++ backend/iweb-gym-grupo5/src/main/java/iwebgym/repository/SuscripcionRepository.java
@@ -0,0 +1,8 @@
+package iwebgym.repository;
+
+import iwebgym.model.Suscripcion;
+import iwebgym.model.TipoActividad;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SuscripcionRepository extends JpaRepository<Suscripcion, Long> {
+}
---
+++ backend/iweb-gym-grupo5/src/main/java/iwebgym/service/CuotaService.java
@@ -0,0 +1,80 @@
+package iwebgym.service;
+
+import iwebgym.model.Moroso;
+import iwebgym.model.Socio;
+import iwebgym.model.Suscripcion;
+import iwebgym.repository.MorosoRepository;
+import iwebgym.repository.SocioRepository;
+import iwebgym.repository.SuscripcionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+import java.util.List;
+
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+@Service
+public class CuotaService {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private SocioRepository socioRepository;
+    @Autowired
+    private SuscripcionRepository suscripcionRepository;
+    @Autowired
+    private MorosoRepository morosoRepository;
+
+    public float get_precio_cuota(String nombre_cuota){
+        List<Suscripcion> suscripcions = suscripcionRepository.findAll();
+        for (Suscripcion suscripcion: suscripcions){
+            if (suscripcion.getTipoSuscripcion().equals(nombre_cuota)){
+                return suscripcion.getPrice();
+            }
+        }
+        return 0;
+    }
+    public void descontar_saldo(Socio socio, float price_cuota){
+        if(socio.getSaldo() < price_cuota){
+            Moroso nuevoMoroso = new Moroso();
+            nuevoMoroso.setId_socio(socio.getId());
+
+            ZoneId madridZone = ZoneId.of("Europe/Madrid");
+            LocalDate currentDate = LocalDate.now(madridZone);
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-yyyy");
+            String fechaMesAño = currentDate.format(formatter);
+
+            nuevoMoroso.setMensualidad_no_pagada(fechaMesAño);
+
+            morosoRepository.save(nuevoMoroso);
+            socio.setActivo(false);
+        }
+        else{
+            float saldo_usuario = socio.getSaldo();
+
+            socio.setSaldo(saldo_usuario - price_cuota);
+        }
+
+        socioRepository.save(socio);
+
+    }
+
+    @Scheduled(cron = "0 0 0 1 * *")  // Esto ejecuta el método el 1er día de cada mes a las 00:00
+    public void descontarCuota() {
+        LocalDate fechaActual = LocalDate.now();
+        if (fechaActual.getDayOfMonth() == 14) {
+            List<Socio> lista_socios = socioRepository.findAll();
+
+            for (Socio socio : lista_socios){
+                if (socio.getActivo()){
+                    String cuota_usario = socio.getTipoCuota();
+                    float price_cuota = get_precio_cuota(cuota_usario);
+                    descontar_saldo(socio, price_cuota);
+                }
+            }
+        }
+    }
+}
