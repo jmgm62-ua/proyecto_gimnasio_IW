175eec28585f70fb9b3732972d3dc5781149b606
Adrián <arr140@alu.ua.es>
2025-01-15 23:52:42 +0100

    Corregido Mis reservas

--- frontend/iweb-gym-frontend/src/views/MisReservas-socio.vue
+++ frontend/iweb-gym-frontend/src/views/MisReservas-socio.vue
@@ -40,7 +40,7 @@
                    class="card shadow-sm">
                 <div class="card-body">
                   <h3 class="h5 mb-3">
-                    <i class="bi bi-bookmark"></i> {{ reserva.titulo }}
+                    <i class="bi bi-bookmark"></i> {{ reserva.nombreActividad }}
                   </h3>
                   <div class="d-flex justify-content-between align-items-center">
                     <div>
