4020fe088c83f92fd16b08f71ce1ddd6ea0d1bf7
Adrián <arr140@alu.ua.es>
2025-01-15 21:10:04 +0100

    Cambiado docker compose para meter el vue en el

--- frontend/iweb-gym-frontend/vite.config.js
+++ frontend/iweb-gym-frontend/vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  server: {
+    host: '0.0.0.0', // Allow access from outside the container
+    port: 5173,      // Ensure the port matches
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
