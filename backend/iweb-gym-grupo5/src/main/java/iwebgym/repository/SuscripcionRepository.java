18de6e0cfde6b5533c1fe0d8c4afd4cf34a52d2f
Nicolás Martín <nmm144@gcloud.ua.es>
2025-01-15 21:37:49 +0100

    Arreglando ids con problema de id-inscripcion

--- backend/iweb-gym-grupo5/src/main/java/iwebgym/repository/SuscripcionRepository.java
+++ backend/iweb-gym-grupo5/src/main/java/iwebgym/repository/SuscripcionRepository.java
@@ -1,8 +1,16 @@
 package iwebgym.repository;
 
+import iwebgym.model.Socio;
 import iwebgym.model.Suscripcion;
 import iwebgym.model.TipoActividad;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface SuscripcionRepository extends JpaRepository<Suscripcion, Long> {
+
+    @Query("SELECT s.id FROM Suscripcion s WHERE s.tipoSuscripcion = :nombreSuscripcion")
+    Long findIdByNombreSuscripcion(@Param("nombreSuscripcion") String nombreSuscripcion);
 }
